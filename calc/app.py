aa04047f0b33ce3226364c57ce8d450486a39982
John Curry <jtc3852@gmail.com>
2021-01-28 20:14:07 -0500

    Added calc

--- calc/app.py
+++ calc/app.py
@@ -1,1 +1,49 @@
-# Put your app in here.
+from flask import Flask
+app = Flask(__name__)
+
+from operations import *
+from flask import request
+
+
+@app.route("/add")
+def add_route():
+  a = int(request.args.get("a"))
+  b = int(request.args.get("b"))
+  result = add(a,b)
+  return str(result)
+
+@app.route("/sub")
+def sub_route():
+  a = int(request.args.get("a"))
+  b = int(request.args.get("b"))
+  result = sub(a,b)
+  return str(result)
+
+@app.route("/mult")
+def mult_route():
+  a = int(request.args.get("a"))
+  b = int(request.args.get("b"))
+  result = mult(a,b)
+  return str(result)
+
+@app.route("/div")
+def div_route():
+  a = int(request.args.get("a"))
+  b = int(request.args.get("b"))
+  result = div(a,b)
+  return str(result)
+
+
+operationDict = {
+  "add":add,
+  "sub":sub,
+  "mult":mult,
+  "div":div
+}
+
+@app.route("/math/<operation>")
+def all_math_operations(operation):
+  a = int(request.args.get("a"))
+  b = int(request.args.get("b"))
+  result = operationDict[operation](a,b)
+  return str(result)
